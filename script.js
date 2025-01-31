d75e0eed3098be809259663bfa5aae65a008c787
Croonchie <salazarjoshua630@gmail.com>
2025-01-31 12:09:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let number = 5; // Assign any number here
+ 
+if (number > 0) {
+    console.log("The number is positive");
+} else if (number < 0) {
+    console.log("The number is negative");
+} else {
+    console.log("The number is zero");
+}
+
+let day = number; //any number here
+
+switch (day) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);  
+}
+
+let i = 1;
+
+while (i <= 5) {
+    console.log(i); 
+    i++;  
+}
+
+let j = 1;
+
+do {
+    console.log(j);  
+    j++;  
+} while (j <= 5);
+
+if (i === <3>) {
+    break;
+}
+
+if (i === <3>) {
+    continue;
+}
